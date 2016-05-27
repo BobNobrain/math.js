beafe7ed0f161d395990d41790e13935c6fb429f
Nikita <nikita-ivanov.mail@yandex.ru>
2016-05-27 16:14:13 +0300

    Created a repo for project. Now ready: - simple JS implementation with injected variables; - console history with ctrl+arrows; - hilighting for different types and js keywords; - recursive dumping of objects and arrays (with no limit);

---
+++ console.js
@@ -0,0 +1,355 @@
+var mcons=null;
+var globalContext=null;
+var inputHistory={
+	inputs: [], current: 0, cache: null,
+	push: function(code) { this.inputs.push(code); this.current=this.inputs.length; },
+	pushCache: function(code) { this.cache=code; },
+	back: function()
+	{
+		if(this.current==0) return null;
+		return this.inputs[--this.current];
+	},
+	forward: function()
+	{
+		if(this.current==this.inputs.length)
+		{
+			return null;
+		}
+		if(this.current==this.inputs.length-1)
+		{
+			++this.current;
+			return null;
+		}
+		return this.inputs[++this.current];
+	},
+	isAtEnd: function() { return this.current==this.inputs.length; }
+};
+
+window.addEventListener('load', function()
+{
+	window.state = {
+		dragging: false
+	};
+	window.elements = {
+		dragger: document.getElementById('dragger'),
+		topPanel: document.getElementById('output-container'),
+		bottomPanel: document.getElementById('input'),
+		text: document.getElementById('command'),
+		output: document.getElementById('output'),
+		outputScroller: document.getElementById('output-clone')
+	};
+
+	window.elements.dragger.addEventListener('mouseDown', function(ev)
+	{
+
+	});
+
+	document.addEventListener('mouseUp', function(ev)
+	{
+		var up = document.getElementById('output-container');
+		var down = document.getElementById('input');
+	});
+
+	window.elements.dragger.addEventListener('doubleclick', function()
+	{
+
+	});
+
+	window.elements.text.addEventListener('keypress', function(ev)
+	{
+		if(ev.keyCode == 13 && !(ev.shiftKey || ev.ctrlKey || ev.altKey))
+		{
+			var code = this.value;
+			inputHistory.push(code);
+
+			var codeText=code.replace(/\n/g, '<br />').replace(/  /g, '&nbsp ').replace(/\t/g, '&nbsp&nbsp&nbsp ');
+			addMessage(createMessage({type: 'input', text: codeText}));
+
+			var result;
+			try
+			{
+				result=execute(code, globalContext);
+				mcons.print(result);
+				result=null;
+			}
+			catch(ex)
+			{
+				mcons.error(ex);
+			}
+			this.value='';
+
+			ev.preventDefault();
+			return false;
+		}
+		if(ev.keyCode == 38 && ev.ctrlKey)
+		{
+			// up in history
+			if(inputHistory.isAtEnd()) inputHistory.pushCache(this.value);
+			var b=inputHistory.back();
+			if(b!==null) this.value=b;
+		}
+		if(ev.keyCode == 40 && ev.ctrlKey)
+		{
+			// down in history
+			var f=inputHistory.forward();
+			if(f===null) this.value=inputHistory.cache;
+			else this.value=f;
+		}
+	});
+
+	function execute(code, context)
+	{
+		/*code=code.replace(/["\\]/g, '\\$1')
+			.replace(/('.*?[^\\])(\\\n)(.*?)'/g, '$1$3')
+			.replace()
+			.replace();*/
+		var props = Object.getOwnPropertyNames(context);
+		var values = props.map(function(item){ return context[item]; });
+
+		props.push('_executee');
+		values.push(code);
+
+		var point = 'return eval(_executee);';
+		return (new Function(props.join(','), point)).apply({}, values);
+	}
+
+	function createMessage(params)
+	{
+		params.__proto__={
+			type: 'string',
+			text: '',
+			// mb: moreValues: [],
+			input: null,
+			typename: ''
+		};
+
+		var div=document.createElement('div');
+		var dt=new Date();
+		div.classList.add('item');
+		div.classList.add(params.type);
+
+		var inner=document.createElement('div');
+		inner.classList.add('time');
+		inner.innerHTML=dt.toLocaleTimeString();
+		div.appendChild(inner);
+
+		inner=document.createElement('div');
+		inner.classList.add('date');
+		var m=dt.getMonth()+1;
+		if(m<10) m='0'+m;
+		inner.innerHTML=dt.getDate()+'.'+m;
+		div.appendChild(inner);
+
+		inner=document.createElement('div');
+		inner.classList.add('type');
+		inner.innerHTML=params.typename;
+		div.appendChild(inner);
+
+		if(params.input!=null)
+		{
+			inner=document.createElement('div');
+			inner.classList.add('content');
+			inner.classList.add('input');
+			inner.innerHTML=params.input;
+			div.appendChild(inner);
+		}
+
+		inner=document.createElement('div');
+		inner.classList.add('content');
+		inner.classList.add(params.type);
+
+		try
+		{
+			inner.appendChild(params.text);
+		}
+		catch(ex)
+		{
+			inner.innerHTML=params.text+'';
+		}
+
+		div.appendChild(inner);
+		return div;
+	}
+
+	function addMessage(msg)
+	{
+		window.elements.output.appendChild(msg);
+		var o=window.elements.outputScroller;
+		o.scrollTo(0, o.scrollHeight-o.clientHeight);
+	}
+
+	mcons={
+		print: function(obj)
+		{
+			if(arguments.length==0) return;
+
+			({
+				// TODO: clean up all the cases except 'object' (they're minded in formOutput)
+				'string': function(str)
+				{
+					addMessage(createMessage({type: 'string', text: fmt.whitespaces(str), typename: 'String'}))
+				},
+				'number': function(n)
+				{
+					addMessage(createMessage({type: 'number', text: n, typename: 'Number'}))
+				},
+				'object': function(obj)
+				{
+					addMessage(createMessage({type: 'object', text: formOutput(obj).render(),
+											 typename: getTypename(obj)}));
+				},
+				'undefined': function()
+				{
+					addMessage(createMessage({type: 'object', text: '(undefined)'}))
+				},
+				'boolean': function(b)
+				{
+					addMessage(createMessage({type: 'bool', text: b, typename: 'Boolean'}))
+				},
+				'function': function(f)
+				{
+					addMessage(createMessage({type: 'code', text: formOutput(f).render(), typename: 'Function'}))
+				}
+			})[typeof obj](obj);
+		},
+		error: function(msg)
+		{
+			addMessage(createMessage({type: 'error', text: msg}))
+		},
+		warning: function(msg)
+		{
+			addMessage(createMessage({type: 'warning', text: msg}))
+		},
+		clear: function()
+		{
+			window.elements.output.innerHTML='';
+		}
+	};
+
+	globalContext={
+		print: mcons.print,
+		warn: mcons.warning,
+		error: mcons.error,
+		help: 	'This is simple JS console with some built-in math libraries and functions.\n' +
+				'Hotkeys: Enter runs the script, Ctrl|Shift|Alt+Enter breaks the line, ' +
+				'Ctrl+Up and Ctrl+Down navigates you through input history.\n' +
+				'Use lookAround() to list all properties in the context.\n' +
+				'Also, all global variables are visible for this console.',
+		lookAround: (function()
+		{
+			var props=Object.getOwnPropertyNames(this);
+			return 'Available items:\n'+props.map(function(item)
+			{
+			  return item+': '+typeof this[item];
+			}, this).join('\n');
+		})//.bind(globalContext)
+	};
+	globalContext.lookAround=globalContext.lookAround.bind(globalContext);
+});
+
+function getTypename(obj)
+{
+	if(typeof obj.typename == typeof '') return obj.typename;
+	var str=({}).toString.call(obj);
+	return str.substring(8, str.length-1);
+}
+
+function formOutput(obj)
+{
+	var f=({
+		'object': function(obj)
+		{
+			if(obj==null)
+			{
+				return new FormattedText().bold("null");
+			}
+			if(typeof obj.render == typeof eval) return obj;
+
+			var output=new FormattedText();
+			if(Array.isArray(obj))
+			{
+				output.plain('[');
+				for(var i=0; i<obj.length; i++)
+				{
+					if(i==obj.length-1) output.startIndent();
+					else output.startList();
+					output.push(formOutput(obj[i]));
+					output.endBlock();
+				}
+				output.plain(']');
+				return output;
+			}
+
+			output.plain('{');
+			Object.getOwnPropertyNames(obj).forEach(function(item, i, arr)
+			{
+				if(i==arr.length-1) output.startIndent();
+				else output.startList();
+
+				output.plain(item+': ');
+				output.push(formOutput(obj[item]));
+
+				output.endBlock();
+			});
+			output.plain('}')
+
+			return output;
+		},
+		'string': function(str)
+		{
+			return new FormattedText().string('"'+str+'"');
+		},
+		'number': function(n)
+		{
+			return new FormattedText().number(n+'');
+		},
+		'undefined': function()
+		{
+			return new FormattedText().plain('(undefined)');
+		},
+		'boolean': function(b)
+		{
+			return new FormattedText().bool(b+'');
+		},
+		'function': function(f)
+		{
+			var code=fmt.escape(f+'');
+			var keywords=['class',
+				'function', 'this', 'new',
+				'return',
+				'if', 'for', 'while', 'do', 'else',
+				'switch', 'null'
+			];
+			keywords.forEach(function(item)
+			{
+				code=code.replace(new RegExp('([^A-z0-9_$]|^)('+item+')([^A-z0-9_$]|$)', 'g'),
+								  '$1<span class="keyword1">$2</span>$3');
+			});
+			keywords=['break', 'continue',
+				'try', 'catch', 'finally',
+				'case', 'default', 'typeof', 'in',
+				'\\[native code\\]'
+			];
+
+			keywords.forEach(function(item)
+			{
+				code=code.replace(new RegExp('([^A-z0-9_$]|^)('+item+')([^A-z0-9_$]|$)', 'g'),
+								  '$1<span class="keyword2">$2</span>$3');
+			});
+
+			keywords=['var', 'let', 'const', 'false', 'true'];
+			keywords.forEach(function(item)
+			{
+				code=code.replace(new RegExp('([^A-z0-9_$]|^)('+item+')([^A-z0-9_$]|$)', 'g'),
+								  '$1<span class="keyword3">$2</span>$3');
+			});
+			console.log(code);
+			return new FormattedText().html(fmt.whitespaces(code));
+		}
+	})[typeof obj];
+	return (f || function(obj)
+	{
+		//default
+		return obj;
+	})(obj);
+}
---
+++ formatting.js
@@ -0,0 +1,209 @@
+var fmt={};
+
+fmt.tag=function(name, content, classes)
+{
+	var el=document.createElement(name);
+	if(typeof content == typeof '') el.innerHTML=content;
+	else if(Array.isArray(content)) content.forEach(function(item)
+	{
+		el.appendChild(item);
+ 	});
+	else el.appendChild(content);
+
+	if(Array.isArray(classes)) classes.forEach(function(item)
+	{
+		el.classList.add(item);
+	});
+	return el;
+};
+
+fmt.bold=function(c)		{ return fmt.tag('b', c); };
+fmt.italic=function(c)		{ return fmt.tag('i', c); };
+fmt.underlined=function(c)	{ return fmt.tag('u', c); };
+fmt.stroked=function(c)		{ return fmt.tag('s', c); };
+fmt.string=function(c)		{ return fmt.tag('span', c, ['string']); };
+fmt.number=function(c)		{ return fmt.tag('span', c, ['number']); };
+fmt.bool=function(c)		{ return fmt.tag('span', c, ['bool']); };
+
+fmt.div=function(content, classes)		{ return fmt.tag('div', content, classes); };
+fmt.span=function(content, classes)		{ return fmt.tag('span', content, classes); };
+
+fmt.br=function() { return document.createElement('br'); };
+fmt.tab=function() { return '&nbsp;&nbsp;&nbsp; '; };
+
+fmt.whitespaces=function(text)
+{
+	return text.replace(/\n/g, '<br />').replace(/  /g, '&nbsp; ').replace(/\t/g, '&nbsp;&nbsp;&nbsp; ');
+}
+fmt.escape=function(text)
+{
+	return text.replace(/&/g, '&amp;').replace(/>/g, '&gt;').replace(/</g, 'lt;');
+};
+
+function FormattedText()
+{
+	this.root=new FTEntry({ tag: 'div' });
+	this.pointer=this.root;
+}
+
+function FTEntry(params)
+{
+	this.buffer=[];
+	this.parent=params.parent || null;
+	this.classes=params.classes || [];
+	this.tagName=params.tag || 'div';
+	if(params.content)
+	{
+		this.buffer.push(params.content);
+	}
+}
+FTEntry.prototype.push=function(text)
+{
+	this.buffer.push(text);
+	return this;
+};
+
+FormattedText.prototype.push=function(entry)
+{
+	if(entry.root) // FormattedText
+	{
+		entry.root.buffer.forEach(function(item){
+			this.pointer.push(item);
+		}, this);
+	}
+	else this.pointer.push(entry); // FTEntry
+	return this;
+};
+
+FormattedText.prototype.startBlock=function(tag, classes)
+{
+	var block=new FTEntry({
+		parent: this.pointer,
+		tag: tag,
+		classes: classes
+	});
+	this.pointer.push(block);
+	this.pointer=block;
+	return this;
+};
+FormattedText.prototype.endBlock=function()
+{
+	this.pointer=this.pointer.parent;
+	return this;
+};
+FormattedText.prototype.endAll=function()
+{
+	this.pointer=this.root;
+	return this;
+};
+FormattedText.prototype.startIndent=function()
+{
+	return this.startBlock('div', ['left-indent']);
+};
+FormattedText.prototype.startList=function()
+{
+	return this.startBlock('div', ['left-indent', 'comma-after']);
+};
+
+FormattedText.prototype.block=function(text, classes)
+{
+	this.pointer.push(new FTEntry({
+		tag: 'div', parent: this,
+		content: text, classes: classes
+	}));
+	return this;
+};
+FormattedText.prototype.number=function(text)
+{
+	this.pointer.push(new FTEntry({
+		tag: 'span', parent: this,
+		content: text, classes: ['number']
+	}));
+	return this;
+};
+FormattedText.prototype.string=function(text)
+{
+	this.pointer.push(new FTEntry({
+		tag: 'span', parent: this,
+		content: text, classes: ['string']
+	}));
+	return this;
+};
+FormattedText.prototype.bool=function(text)
+{
+	this.pointer.push(new FTEntry({
+		tag: 'span', parent: this,
+		content: text, classes: ['bool']
+	}));
+	return this;
+};
+
+FormattedText.prototype.bold=function(text)
+{
+	this.pointer.push(new FTEntry({
+		tag: 'b', parent: this,
+		content: text
+	}));
+	return this;
+};
+FormattedText.prototype.italic=function(text)
+{
+	this.pointer.push(new FTEntry({
+		tag: 'i', parent: this,
+		content: text
+	}));
+	return this;
+};
+FormattedText.prototype.underlined=function(text)
+{
+	this.pointer.push(new FTEntry({
+		tag: 'u', parent: this,
+		content: text
+	}));
+	return this;
+};
+FormattedText.prototype.plain=function(text)
+{
+	this.pointer.push(text);
+	return this;
+};
+FormattedText.prototype.html=function(text)
+{
+	this.pointer.push({
+		text: text,
+		render: function()
+		{
+			var el=document.createElement('span');
+			el.innerHTML=this.text;
+			return el;
+		}
+	});
+	return this;
+};
+
+FormattedText.prototype.render=function()
+{
+	return this.root.render();
+};
+
+FTEntry.prototype.render=function()
+{
+	var el=document.createElement(this.tagName);
+	this.classes.forEach(function(item)
+	{
+		el.classList.add(item);
+	});
+	this.buffer.forEach(function(item)
+	{
+		if(typeof item == typeof '')
+		{
+			item=document.createTextNode(item);
+		}
+		else
+		{
+			item=item.render();
+		}
+		el.appendChild(item);
+	});
+	return el;
+};
